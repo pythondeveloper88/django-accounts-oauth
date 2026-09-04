c25e88c5d5770a24a42d89fb390323a90a2af160
pythondeveloper88 <sahra.mazaheri@gmail.com>
2026-09-04 19:54:20 +0330

    Add accounts signals

---
+++ accounts/signals.py
@@ -0,0 +1,23 @@
+"""
+Signals for accounts app.
+"""
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+from .models import UserProfile, LoginAttempt
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """Create UserProfile when User is created."""
+    if created:
+        UserProfile.objects.get_or_create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """Save UserProfile when User is saved."""
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
